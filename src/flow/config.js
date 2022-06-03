480f02e746c36af6edd71320b1e037f02c874fec
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-03 10:42:28 -0400

    configure flow testnet

---
+++ src/flow/config.js
@@ -0,0 +1,9 @@
+import { config } from "@onflow/fcl";
+
+// Use environment variables for easy deployment across environments
+config({
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+  "0xLoveJulesLeather": "0xd8144e7c81e68eb9", // Yes Jules Testnet contract address
+})
+
