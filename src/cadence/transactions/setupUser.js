8c4c028ecb7cb49f2403bbf7434dd784d9267e6e
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-03 11:11:42 -0400

    setup user transaction

---
+++ src/cadence/transactions/setupUser.js
@@ -0,0 +1,18 @@
+export const setupUser = `
+import LoveJulesLeather from 0xLoveJulesLeather
+
+transaction {
+
+  prepare(acct: AuthAccount) {
+    acct.save(<- LoveJulesLeather.createEmptyCollection(), to: /storage/LoveJulesLeatherCollection)
+
+    // Create a public link to users collection
+    acct.link<&LoveJulesLeather.Collection{LoveJulesLeather.CollectionPublic}>(/public/LoveJulesLeather, target: /storage/LoveJulesLeatherCollection )
+  }
+
+  execute {
+    log("A user stored a LoveJulesLeather Collection inside their account")
+  }
+}
+
+`
