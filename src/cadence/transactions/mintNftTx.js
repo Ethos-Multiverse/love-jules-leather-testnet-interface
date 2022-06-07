b7e8bcf46622abd1e3440dd591bbd640e30267e7
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-07 11:15:06 -0400

    update mintNFT tx

--- src/cadence/transactions/mintNftTx.js
+++ src/cadence/transactions/mintNftTx.js
@@ -1,21 +1,14 @@
 export const mintNftTx = `
-import LoveJulesLeather from 0xLoveJulesLeather
-
-transaction {
-    prepare(acct: AuthAccount) {
-        // Get LoveJulesLeather collection in users wallet
-        let collection = acct.borrow<&LoveJulesLeather.Collection>(from: /storage/LoveJulesLeatherCollection)
-                    ?? panic("LoveJulesLeather collection does not exist in users storage")
-
-        // Mint an NFT
-        let nft <- LoveJulesLeather.mint()
-
-        // Store nft inside users LoveJulesLeather collection
-        collection.deposit(token: <- nft)
-    }
-
-    execute {
-        log("A user minted a LoveJuleLeather NFT into their account")
-    }
+import LoveJulesLeatherV1 from 0xLoveJulesLeatherV1
+transaction(ipfsHash: String, name: String) {
+  prepare(acct: AuthAccount) {
+    let collection = acct.borrow<&LoveJulesLeatherV1.Collection>(from: /storage/LoveJulesLeatherV1Collection)
+                        ?? panic("This collection does not exist here")
+    let nft <- LoveJulesLeatherV1.createToken(ipfsHash: ipfsHash, metadata: {"name": name})
+    collection.deposit(token: <- nft)
+  }
+  execute {
+    log("A user minted an NFT into their account")
+  }
 }
 `
