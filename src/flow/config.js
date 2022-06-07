283cfa4a70658735989541c6a82cb9bc04c42168
Wielfried Zouantcha <wiel@zouantcha.com>
2022-06-07 10:51:17 -0400

    add marketplace address to flow config

--- src/flow/config.js
+++ src/flow/config.js
@@ -4,6 +4,7 @@ import { config } from "@onflow/fcl";
 config({
   "accessNode.api": "https://rest-testnet.onflow.org",
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
-  "0xLoveJulesLeather": "0xd8144e7c81e68eb9", // Yes Jules Testnet contract address
+  "0xLoveJulesLeatherV1": "0xd8144e7c81e68eb9", // Yes Jules Testnet contract address
+  "0xLoveJulesLeatherMarketV1": "0xd8144e7c81e68eb9"
 })
 
